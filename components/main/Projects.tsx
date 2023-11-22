9db29994e8eec3b9a9a2fb12a754c3184036b9fc
Santino Mantineo <santinomantineo@gmail.com>
2023-11-22 02:41:29 -0300

    Portfolio finished

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,31 +1,46 @@
+// Projects.tsx
+
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
 
+const projectsData = [
+  {
+    src: "/Locanjeamos.JPG",
+    title: "Lo Canjeamos",
+    description: "Lo Canjeamos 🔄 is a user-friendly web platform and mobile app that tackles the challenge of skyrocketing prices on major e-commerce platforms in Latin America.",
+    repoUrl: "https://github.com/SantinoMantineo/Lo-Canjeamos",
+    webUrl: "https://locanjeamos.com.ar",
+  },
+  {
+    src: "/DogsClub.JPG",
+    title: "Dog's Club",
+    description: "Bring your dream dogs to life with our intuitive creation feature. Customize breeds, colors, and personalities to design your perfect virtual pup.",
+    repoUrl: "https://github.com/SantinoMantineo/PI-Dogs-main",
+  },
+  {
+    src: "/Lamp.JPG",
+    title: "Ligth-Web",
+    description: "The place to discover the latest trends in lighting and transform your spaces with style. Elevate your spaces with sophistication.",
+    repoUrl: "https://github.com/SantinoMantineo/Light-Web",
+  },
+  {
+    src: "/rickymorty.JPG",
+    title: "Rick & Morty",
+    description: "A passion project born from my journey through each bootcamp's module. Create and add Favorites R&M characters!",
+    repoUrl: "https://github.com/SantinoMantineo/Rick_Morty_Vite",
+  },
+];
+
 const Projects = () => {
   return (
-    <div
-      className="flex flex-col items-center justify-center py-20"
-      id="projects"
-    >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+    <div className="flex flex-col items-center justify-center py-20" id="projects">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-2 mb-[25px]">
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/Locanjeamos.JPG"
-          title="Lo Canjeamos"
-          description="Lo Canjeamos 🔄 is a user-friendly web platform and mobile app that tackles the challenge of skyrocketing prices on major e-commerce platforms in Latin America."
-        />
-        <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
+        {projectsData.map((project, index) => (
+          <ProjectCard key={index} {...project} />
+        ))}
       </div>
     </div>
   );
--- components/sub/ProjectCard.tsx
+++ components/sub/ProjectCard.tsx
@@ -5,11 +5,13 @@ interface Props {
   src: string;
   title: string;
   description: string;
+  repoUrl: string;
+  webUrl?: string;
 }
 
-const ProjectCard = ({ src, title, description }: Props) => {
+const ProjectCard = ({ src, title, description, repoUrl, webUrl }: Props) => {
   return (
-    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]">
+    <div className="relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61] z-40">
       <Image
         src={src}
         alt={title}
@@ -20,7 +22,28 @@ const ProjectCard = ({ src, title, description }: Props) => {
 
       <div className="relative p-4">
         <h1 className="text-2xl font-semibold text-white">{title}</h1>
-        <p className="mt-2 text-gray-300">{description}</p>
+        <p className={`mt-2 text-gray-300 ${webUrl ? 'mb-[20px]' : 'mb-[35px]'}`}>{description}</p>
+
+        <a
+          className={`py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px] 
+            ${webUrl ? 'ml-[15%]' : 'ml-[36%]'} pl-[10px] pr-[10px]`}
+          href={repoUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          See Code
+        </a>
+
+        {webUrl && (
+          <a
+            className="py-2 button-primary text-center text-white cursor-pointer rounded-lg pl-[10px] ml-[20%] pr-[10px]"
+            href={webUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            See Live
+          </a>
+        )}
       </div>
     </div>
   );
