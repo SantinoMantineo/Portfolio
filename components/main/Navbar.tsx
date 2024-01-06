07513c30d3cc17036815e21aa2b21d53ab6c8cc9
Santino Mantineo <santinomantineo@gmail.com>
2024-01-06 06:28:07 -0300

    Navbar Responsibe

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -4,9 +4,9 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
-      <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px]">
-        <a
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-4 md:px-10">
+      <div className="w-full h-full flex flex-col md:flex-row items-center justify-between m-auto">
+      <a
           href="#about-me"
           className="h-auto w-auto flex flex-row items-center hidden md:flex" // Muestra en pantallas medianas y más grandes
         >
@@ -15,32 +15,29 @@ const Navbar = () => {
             alt="logo"
             width={50}
             height={50}
-            className="cursor-pointer hover:animate-slowspin mr-[15px]"
+            className="cursor-pointer hover:animate-slowspin mr-2 md:mr-[15px]"
           />
 
-          <span className="font-bold ml-[25px] text-gray-300">
+          <span className="font-bold text-gray-300">
             Santino Mantineo
           </span>
         </a>
-
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <a href="#about-me" className="cursor-pointer">
-              About me
-            </a>
-            <a href="#skills" className="cursor-pointer">
-              Skills
-            </a>
-            <a href="#experience" className="cursor-pointer">
-              Experience
-            </a>
-            <a href="#projects" className="cursor-pointer">
-              Projects
-            </a>
-          </div>
+        <div className="flex items-center justify-between w-full md:w-[500px] h-auto border border-[#7042f861] bg-[#0300145e] md:mr-0 mr-[15px] md:px-[20px] py-[10px] my-[10px] pl-[10px] pr-[10px] rounded-full text-gray-200 mb-2 md:mb-0">
+          <a href="#about-me" className="cursor-pointer">
+            About me
+          </a>
+          <a href="#skills" className="cursor-pointer">
+            Skills
+          </a>
+          <a href="#experience" className="cursor-pointer">
+            Experience
+          </a>
+          <a href="#projects" className="cursor-pointer">
+            Projects
+          </a>
         </div>
 
-        <div className="flex flex-row gap-5">
+        <div className="flex flex-row gap-2 md:gap-5 mt-2 md:mt-0">
           {Socials.map((social) => (
             <a
               key={social.name}
