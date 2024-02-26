93665fa265ea3b34b26db19e5e8f1f5e2a69d24e
Santino Mantineo <santinomantineo@gmail.com>
2024-02-26 18:35:16 -0300

    Add Anta font and update InitialScreen component. Added quote

--- components/main/InitialScreen.tsx
+++ components/main/InitialScreen.tsx
@@ -5,8 +5,6 @@ const InitialScreen = (props: any) => {
   const { setShowContent } = props;
   const [fadeOutActive, setFadeOutActive] = useState(false);
 
-  // que haya una animacion, estimar el tiempo y hacer que transcurrido el tiempo se avance a la home page.
-
   const handleSkipIntro = () => {
     setFadeOutActive(true);
     setShowContent();
@@ -14,11 +12,13 @@ const InitialScreen = (props: any) => {
 
   return (
     <div
-      className={`${styles.initialScreen} ${fadeOutActive && styles.fadeOut}`}
+      className={`${styles.initialScreen} ${fadeOutActive && styles.fadeOut} fade-in flex items-center justify-center`}
     >
-      <div>
+      <div className="text-center">
         {/* Contenido de tu animación aquí */}
-        <button className={`${styles.skipButton} text-6xl font-bold text-white"`} onClick={handleSkipIntro}>
+        <h1 className="anta text-7xl font-bold text-white mb-10">"The only way to do great work is to love what you do"</h1>
+        <h2 className="Welcome-text text-3xl text-white">- Steve Jobs</h2>
+        <button className={`${styles.skipButton} text-6xl font-bold text-white`} onClick={handleSkipIntro}>
           SKIP INTRO
         </button>
       </div>
