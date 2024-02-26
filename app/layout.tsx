6cb591229c11ba9a1402c612b83c455dcb68059c
Santino Mantineo <santinomantineo@gmail.com>
2024-02-26 16:23:43 -0300

    Initial Layout with fade in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,47 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 import Footer from "@/components/main/Footer";
+import InitialScreen from "@/components/main/InitialScreen";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Santino's Portfolio",
-  description: "This is my portfolio",
-};
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowContent(true);
+    }, 5000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <html lang="en">
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
-        <StarsCanvas />
-        <Navbar />
-        {children}
-        <Footer />
+        {showContent ? (
+          <div className="fade-in">
+            <StarsCanvas />
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        ) : (
+          <InitialScreen />
+        )}
       </body>
     </html>
   );
---
+++ components/main/InitialScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./InitialScreen.module.css";
+
+const InitialScreen = () => {
+  return <div className={styles.initialScreen}>
+
+  </div>;
+};
+
+export default InitialScreen;
