d56515a9bf10d62411da1b72fda86b4ec96f7741
Santino Mantineo <santinomantineo@gmail.com>
2024-02-27 18:18:03 -0300

    Update CSS styles for the initial screen and hero content components

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -22,7 +22,7 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex flex-row items-center justify-center px-4 sm:px-23 mt-40 w-full z-[20] lg:ml-20" // Ajuste de margen para pantallas grandes
+      className="flex flex-row items-center justify-center px-4 sm:px-23 mt-40 w-full z-[20] lg:ml-20"
     >
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
