51c89edfce5a125fc633f29fb8ced8b3a9563eb9
Santino Mantineo <santinomantineo@gmail.com>
2024-02-27 03:24:34 -0300

    Update the intro screen text

--- components/main/Initial_Screen/InitialScreen.tsx
+++ components/main/Initial_Screen/InitialScreen.tsx
@@ -18,7 +18,7 @@ const InitialScreen = (props: any) => {
     }, 10100);
 
     return () => clearTimeout(timerId);
-  }, [setShowContent]);
+  }, [handleSkipIntro, setShowContent]);
 
   return (
     <div className={`${styles.initialScreen} ${fadeOutActive && styles.fadeOut} fade-in`}>
@@ -27,10 +27,10 @@ const InitialScreen = (props: any) => {
         <StarsCanvas />
         <div className={`${styles.textContainer} text-center`}>
           <h1 className="anta text-6xl font-bold text-white mb-10 max-w-[700px]">
-            "Programming isn't about what you
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">{" "}know;{" "}</span>
-            it's about what you can
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">{" "}figure out</span>"
+          "Programming isn't about what you
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">{` `}know;{` `}</span>
+          it's about what you can
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">{` `}figure out</span>"
           </h1>
           <h2 className="Welcome-text text-3xl text-white">- Chris Pine</h2>
         </div>
