3e3c341a9eb263b407e6a1eb159ffe191417a345
Santino Mantineo <santinomantineo@gmail.com>
2024-02-27 18:48:09 -0300

    changed the bottom and top bar sized on smaller screens

--- components/main/Initial_Screen/InitialScreen.tsx
+++ components/main/Initial_Screen/InitialScreen.tsx
@@ -26,13 +26,13 @@ const InitialScreen = (props: any) => {
       <div className={styles.media}>
         <StarsCanvas />
         <div className={`${styles.textContainer} text-center`}>
-          <h1 className="anta text-6xl font-bold text-white mb-10 max-w-[700px] sm: text-3xl md: text-4xl">
+          <h1 className="anta text-6xl font-bold text-white mb-10 max-w-[700px] sm: text-4xl">
             {"Programming isn't about what you"}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">{" know; "}</span>
             {"it's about what you can"}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">{" figure out"}</span>
           </h1>
-          <h2 className="Welcome-text text-3xl text-white">- Chris Pine</h2>
+          <h2 className="Welcome-text text-3xl text-white sm: text-2xl">- Chris Pine</h2>
         </div>
         {/* <div className={`${styles.modelContainer} w-full h-full`}></div> */}
       </div>
